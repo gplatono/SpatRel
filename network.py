2c9a591e03433a08ad926170fad03a28d8f8bc08
Yifei Yang <43098177+Franklee2017@users.noreply.github.com>
2020-07-10 10:09:33 -0400

    added multi-label model

--- network.py
+++ network.py
@@ -5,12 +5,10 @@ from torch.utils.data import DataLoader
 import json
 
 
-num_classes = 14
+num_classes = 15
 
 class Dataset(torch.utils.data.Dataset):
   def __init__(self, samples, labels):
-        print (labels)
-        print (samples)
         self.labels = labels
         self.samples = samples
 
@@ -20,8 +18,9 @@ class Dataset(torch.utils.data.Dataset):
   def __getitem__(self, index):
     #print ('item: ', self.samples[index], self.labels[index])
     return self.samples[index], self.labels[index]
-        
-def build_data_loader(batch_size, num_workers, is_shuffle, relation='above'):
+
+
+def build_data_loader1(batch_size, num_workers, is_shuffle, relation):
 
     rel_to_label = {'on': 14, 'to the left of': 1, 'left of': 1, 'to the right of': 2, 'right of': 2, 'above': 3,
             'below': 4, 'in front of': 5, 'behind': 6, 'over': 7, 'under': 8, 'underneath': 8, 'in': 9, 'inside': 9,
@@ -36,8 +35,6 @@ def build_data_loader(batch_size, num_workers, is_shuffle, relation='above'):
     samples = []
     labels = []
     for item in data:
-        #label = [0] * num_classes
-        #label[item['label']-1] = 1
         if item['label'] == relation:
             label = [1]
         elif item['label'] == -relation:
@@ -52,10 +49,76 @@ def build_data_loader(batch_size, num_workers, is_shuffle, relation='above'):
             sample += arg[:3]
         #sample += item['observer_loc']
         #sample += item['observer_front']
-        print (sample)
+        print('samples: ', sample)
         samples.append(torch.tensor(sample, dtype=torch.float32))
-    print (len(labels))
-    input()
+    print('labels: ', labels)
+    print('label count: ', len(labels))
+    #input()
+    dataset = Dataset(samples, labels)
+    train_loader = DataLoader(dataset, batch_size = batch_size, shuffle = is_shuffle, num_workers = num_workers)
+    test_loader = train_loader
+    return train_loader, test_loader
+
+
+# data loader for multi-class
+def build_data_loader2(batch_size, num_workers, is_shuffle):
+    data = None
+    with open('dataset', 'r') as file:
+        data = file.readlines()
+    data = [json.loads(item) for item in data]
+    samples = []
+    labels = []
+    for item in data:
+        label = [0] * num_classes
+        if item['label'] > 0 and item['label'] != 12:
+            label[item['label']-1] = 1
+            labels.append(torch.tensor(label, dtype=torch.float32))
+            sample = []
+            for arg in item['arg0']:
+                sample += arg[:3]
+            for arg in item['arg1']:
+                sample += arg[:3]
+            print('samples: ', sample)
+            samples.append(torch.tensor(sample, dtype=torch.float32))
+        else:
+            continue
+
+    print('labels: ', labels)
+    print('label count: ', len(labels))
+    dataset = Dataset(samples, labels)
+    train_loader = DataLoader(dataset, batch_size = batch_size, shuffle = is_shuffle, num_workers = num_workers)
+    test_loader = train_loader
+    return train_loader, test_loader
+
+
+# data loader for multi-label
+def build_data_loader3(batch_size, num_workers, is_shuffle):
+    data = None
+    with open('dataset', 'r') as file:
+        data = file.readlines()
+    data = [json.loads(item) for item in data]
+    samples = []
+    labels = []
+    for item in data:
+        label = [0] * num_classes
+        if item['label'] != 12 and item['label'] != -12:
+            if item['label'] > 0:
+                label[item['label']-1] = 1
+            else:
+                label[item['label']-1] = 0
+            labels.append(torch.tensor(label, dtype=torch.float32))
+            sample = []
+            for arg in item['arg0']:
+                sample += arg[:3]
+            for arg in item['arg1']:
+                sample += arg[:3]
+            print('samples: ', sample)
+            samples.append(torch.tensor(sample, dtype=torch.float32))
+        else:
+            continue
+
+    print('labels: ', labels)
+    print('label count: ', len(labels))
     dataset = Dataset(samples, labels)
     train_loader = DataLoader(dataset, batch_size = batch_size, shuffle = is_shuffle, num_workers = num_workers)
     test_loader = train_loader
@@ -68,19 +131,19 @@ class Net1(nn.Module):
         self.n_in = n_in
         self.n_out = n_out
         self.hidden1 = nn.Linear(n_in, 10)
-        #self.hidden_1 = nn.Linear(10, 5)
-        self.output = nn.Linear(10, n_out)
-        #self.sigmoid = nn.Sigmoid()
+        self.hidden2 = nn.Linear(10, 5)
+        self.output = nn.Linear(5, n_out)
+        self.softmax = nn.Softmax(dim=1)  # used for mutli-label
 
     def forward(self, z):
         z = self.hidden1(z)
-        #z = self.hidden_1(z)
+        z = self.hidden2(z)
         z = self.output(z)
-        #z = self.sigmoid(z)
+        z = self.softmax(z)
         return z
 
     
-# generalized version
+# generalized version of base-line model
 class Net2(nn.Module):
     def __init__(self, n_in, n_out, h_shapes=[]):
         super(Net2, self).__init__()
@@ -102,6 +165,7 @@ class Net2(nn.Module):
 
     
 def train(train_loader, net, optimizer, criterion, epoch):
+    print("start training")
     epoch_loss = 0.0
     for epoch_i in range(epoch):
         # print ("INIT")
@@ -115,16 +179,15 @@ def train(train_loader, net, optimizer, criterion, epoch):
             inputs, labels = data
             optimizer.zero_grad()
             outputs = net(inputs)
-            #labels = torch.max(labels, 1)[1]
-            print ('outputs: ', outputs)
-            print ('labels: ', labels)
+            # labels = torch.max(labels, 1)[1]  # transfer label to 1D target when using CrossEntropy
+            print('outputs: ', outputs)
+            print('labels: ', labels)
             rnd = torch.round(outputs)
-            print ('rounded outputs: ', rnd)
+            print('rounded outputs: ', rnd)
             acc = torch.round(torch.abs(outputs - labels))
             acc = 1 - torch.sum(acc) / torch.numel(acc)
-            print ('batch acc: ', acc * 100)
+            print('batch acc: ', acc * 100)
             epoch_acc += acc
-            # loss = criterion(outputs, torch.max(labels, 1)[1])
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -139,7 +202,7 @@ def test(test_loader, net):
     correct = 0
     total = 0
     with torch.no_grad():
-        for data in enumerate(test_loader):
+        for step, data in enumerate(test_loader):
             inputs, labels = data
             outputs = net(inputs)
             _, predicted = torch.max(outputs.data, 1)
@@ -152,16 +215,15 @@ def test(test_loader, net):
 
 if __name__ == '__main__':
     # build dataloader
-    train_loader, test_loader = build_data_loader(batch_size=10, num_workers=1, is_shuffle=True, relation='near')
+    # train_loader, test_loader = build_data_loader(batch_size=10, num_workers=1, is_shuffle=True, relation='near')
     """See rel_to_label dictionary for the set of options for 'relation' """
-
+    train_loader, test_loader = build_data_loader3(batch_size=10, num_workers=1, is_shuffle=True)
     # initialize model
-    net = Net1(n_in = 6, n_out = 1)
-    #net = Net2(n_in=72, n_out=1, h_shapes=[50, 40])
-    # use crossentropy loss
-    #criterion = nn.CrossEntropyLoss()
-    #criterion = nn.BCEWithLogitsLoss()
-    criterion = nn.MSELoss()
+    net = Net1(n_in=6, n_out=15)
+    # net = Net2(n_in=72, n_out=1, h_shapes=[50, 40])
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCEWithLogitsLoss()
+    # criterion = nn.MSELoss()
     # use sgd optimizer
     optimizer = optim.Adam(net.parameters(), lr=0.001)
     # train
