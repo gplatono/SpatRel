5db316879c03b5d1ea08a21de7a9bb3b7cab25fc
gplatono <gr.plat@gmail.com>
2020-10-16 14:14:57 -0400

    empty

--- train_scene.py
+++ train_scene.py
@@ -5,7 +5,7 @@ import os
 # print (sys.executable)
 filepath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, filepath)
-sys.path.append('/home/georgiy/.local/lib/python3.7/site-packages/')
+#sys.path.append('/home/georgiy/.local/lib/python3.7/site-packages/')
 
 from world import World
 from spatial2 import Spatial
@@ -18,14 +18,9 @@ world = World(bpy.context.scene, simulation_mode=True)
 spatial = Spatial(world)
 
 with open(datapath, "r") as f:
-	#print ("DATAPATH: ", datapath)
 	lines = f.readlines()
-	# annotations = []
-	# for line in lines:
-	# 	if line.strip() != "":
-	# 		annotations.append(line)
 	annotations = [line.split(":") for line in lines if line.strip() != ""]
 	#spatial.reload(world)
-	spatial.train(annotations, 10)
+	spatial.train(annotations, 100)
 	spatial.save_parameters()
 	bpy.ops.wm.quit_blender()
